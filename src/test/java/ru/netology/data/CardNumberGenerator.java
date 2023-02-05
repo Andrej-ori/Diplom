070c770ca307991a2400ce208ee34cdaf3f6bab8
Andrej-ori <a31338805@gmail.com>
2023-02-05 16:18:16 +0300

    Удаление TODO

--- src/test/java/ru/netology/data/CardNumberGenerator.java
+++ src/test/java/ru/netology/data/CardNumberGenerator.java
@@ -1,17 +1,11 @@
 package ru.netology.data;
 
-import com.sun.xml.bind.v2.TODO;
-
 import static ru.netology.data.DataHelper.faker;
 
 public class CardNumberGenerator {
-    private CardNumberGenerator() {
-    }
+    private CardNumberGenerator() {}
 
 //    номер одобренной карты
-//    TODO: написать код для обращения к базе данных чтоб номер карты брать из базы
-//          например:
-//          нужен номер одобренной карты = идем в базу, смотрим одобреную карту и берем ее номер
     public static String ApprovedCardNumber() {
         return "4444 4444 4444 4441";
     }
