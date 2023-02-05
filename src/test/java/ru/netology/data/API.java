1e72b97ff5ec648f144fa45b15092c73a8ecee45
Andrej-ori <a31338805@gmail.com>
2023-02-05 16:43:37 +0300

    add API; SQLHelper

---
+++ src/test/java/ru/netology/data/API.java
@@ -0,0 +1,47 @@
+package ru.netology.data;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+
+import static io.restassured.RestAssured.given;
+
+public class API {
+
+    @Value
+    public static class APIResponse {
+        private String status;
+    }
+
+    public static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(8080)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    public static APIResponse getPaymentStatus(DataHelper.CardInfo cardInfo) {
+        return given()
+                .spec(requestSpec)
+                .body(cardInfo)
+                .when()
+                .post("/api/v1/pay")
+                .then()
+                .statusCode(200)
+                .extract().response().as(APIResponse.class);
+    }
+
+    public static API getCreditStatus(DataHelper.CardInfo cardInfo) {
+        return given()
+                .spec(requestSpec)
+                .body(cardInfo)
+                .when()
+                .post("/api/v1/pay")
+                .then()
+                .statusCode(200)
+                .extract().response().as(API.class);
+    }
+}
