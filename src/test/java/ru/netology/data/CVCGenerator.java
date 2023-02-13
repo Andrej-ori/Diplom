325b81173be3086adaaf9cc922b29bfec99a4c42
Andrej-ori <a31338805@gmail.com>
2023-02-13 18:59:59 +0300

    modified Page Failes : Добавление степов с описанием происходящего в шаге

--- src/test/java/ru/netology/data/CVCGenerator.java
+++ src/test/java/ru/netology/data/CVCGenerator.java
@@ -1,7 +1,9 @@
 package ru.netology.data;
 
-import static ru.netology.data.DataHelper.faker;
+import io.qameta.allure.Step;
 import lombok.Value;
+
+import static ru.netology.data.DataHelper.faker;
 
 public class CVCGenerator {
 
@@ -13,17 +15,27 @@ public class CVCGenerator {
     }
 
     // получение кода из 3х цифр
+    @Step("Ввод валидного кода CVC из трех цифр")
     public static CVCNumber getValidCVCNumber() {
         return new CVCNumber(faker.numerify("###"));
     }
 
 //  Получение кода из 2х цифр
+    @Step("Ввод невалидного кода CVC из двух цифр")
     public static CVCNumber getInvalidCVCNumberWithTwoNumbers() {
         return new CVCNumber(faker.numerify("##"));
     }
 
 //  Получение кода из 1 цифры
+    @Step("Ввод невалидного кода CVC из одной цифры")
     public static CVCNumber  getInvalidCVVNumberWithSingleNumber() {
         return new CVCNumber(faker.numerify("#"));
+    }
+
+//    Получение пустой формы
+    @Step("Оставление поля Код CVC пустым")
+    public static CVCNumber getEmptyCVCNumber() {
+        String cvcNumber = "";
+        return new CVCNumber(cvcNumber);
     }
 }
